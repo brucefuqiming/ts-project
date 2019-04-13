48232025caf7402dd040fb5c43983585a172c0d0
guoding <3710490@qq.com>
2019-04-13 15:36:25 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <hello-world :msg="msg" ref="helloworld"></hello-world>
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+  public $refs!: { // strictPropertyInitialization 需要对所有的实际属性赋值 所以需要加感叹号
+    helloworld: HelloWorld,
+  };
+  public isActive: boolean = false;
+  public msg: string = 'test';
+  public created() {
+    console.log('created');
+  }
+
+  public mounted() {
+    this.$refs.helloworld.testMethod();
   }
 }
+</script>
+
+<style lang="scss">
 </style>
